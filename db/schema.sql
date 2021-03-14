402cca1ce0c5769a6050279f1d230a968005875c
Ian Jackson <ijacksondesign@gmail.com>
2021-03-13 22:43:26 -0500

    set up database

---
+++ db/schema.sql
@@ -0,0 +1,39 @@
+DROP DATABASE IF EXISTS `employees_db`;
+CREATE DATABASE `employees_db`;
+USE `employees_db`;
+
+DROP TABLE IF EXISTS `departments`;
+DROP TABLE IF EXISTS `roles`;
+DROP TABLE IF EXISTS `employees`;
+
+CREATE TABLE `departments` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+);
+
+CREATE TABLE `roles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(30) NOT NULL,
+  `salary` decimal(10,0) NOT NULL,
+  `department_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_department_idx` (`department_id`),
+  CONSTRAINT `fk_department` FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`)
+);
+
+CREATE TABLE `employees` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  `role_id` int DEFAULT NULL,
+  `manager_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_role_idx` (`role_id`),
+  KEY `fk_manager_idx` (`manager_id`),
+  CONSTRAINT `fk_role` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`),
+  CONSTRAINT `fk_manager` FOREIGN KEY (`manager_id`) REFERENCES `roles` (`id`)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,48 @@
+INSERT INTO departments (name) 
+VALUES 
+    ('Marketing'),
+    ('Art'),
+    ('Sales'),
+    ('Human Resources');
+
+INSERT INTO roles (title, salary)
+VALUES
+    ('Analyst', 60000),
+    ('Social Media', 50000),
+    ('Coordinator', 56000),
+    ('Designer', 60000),
+    ('Manager', 100000),
+    ('Copy Writer', 58000),   
+    ('UX/UI', 85000),
+    ('Accountant', 55000),
+    ('Intern', 25000),
+    ('Recruiter', 45000),
+    ('Representative', 42000),
+    ('Assistant', 35000);
+
+INSERT INTO employees (first_name, last_name) 
+VALUES 
+    ('James', 'Fraser'),
+    ('Jack', 'London'),
+    ('Robert', 'Bruce'),
+    ('Peter', 'Greenaway'),
+    ('Derek', 'Jarman'),
+    ('Paolo', 'Pasolini'),
+    ('Heathcote', 'Williams'),
+    ('Sandy', 'Powell'),
+    ('Emil', 'Zola'),
+    ('Sissy', 'Coalpits'),
+    ('Antoinette', 'Capet'),
+    ('Samuel', 'Delany'),
+    ('Tony', 'Duvert'),
+    ('Dennis', 'Cooper'),
+    ('Monica', 'Bellucci'),
+    ('Samuel', 'Johnson'),
+    ('John', 'Dryden'),
+    ('Alexander', 'Pope'),
+    ('Lionel', 'Johnson'),
+    ('Aubrey', 'Beardsley'),
+    ('Tulse', 'Luper'),
+    ('William', 'Morris'),
+    ('George', 'Shaw');
+       
